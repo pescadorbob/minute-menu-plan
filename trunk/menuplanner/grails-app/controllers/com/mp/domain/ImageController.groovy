d9256d3072b94f30caa609eb1c3bcea99e7cc5da
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-03-19 13:23:59 +0000

    Change in recipe controller

--- trunk/menuplanner/grails-app/controllers/com/mp/domain/ImageController.groovy
+++ trunk/menuplanner/grails-app/controllers/com/mp/domain/ImageController.groovy
@@ -2,23 +2,13 @@ package com.mp.domain
 
 class ImageController {
 
-  def imgMeasuredProduct= {
+  def recipeImage= {
               if(params.id)
               {
-              MeasuredProduct temp = MeasuredProduct.get( params.id )
-              byte[] image = temp.image
+              RecipeImage recipeImage = RecipeImage.get( params.id )
+              byte[] image = recipeImage.image
               response.outputStream << image
               }
           }
-
-  def imgRecipe= {
-              if(params.id)
-              {
-              Recipe temp = Recipe.get( params.id )
-              byte[] image = temp.image
-              response.outputStream << image
-              }
-          }
-
   def index = { }
 }
