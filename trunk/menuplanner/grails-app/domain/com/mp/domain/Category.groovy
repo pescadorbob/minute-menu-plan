f8835e6c2d26d00d5e8d336d744f8fe3ca37f778
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-03-23 06:31:26 +0000

    Added Category domain classes

---
+++ trunk/menuplanner/grails-app/domain/com/mp/domain/Category.groovy
@@ -0,0 +1,30 @@
+package com.mp.domain
+
+class Category {
+    String name
+    static hasMany = [recipeCategories: RecipeCategory]
+
+    static transients = ['recipes']
+
+    List<Recipe> getRecipes() {
+        return recipeCategories?.collect {it.recipe}
+    }
+
+    List addToRecipes(Recipe recipe) {
+        RecipeCategory.link(recipe, this)
+        return recipes
+    }
+
+    List removeFromRecipes(Recipe recipe) {
+        RecipeCategory.unlink(recipe, this)
+        return recipes
+    }
+
+    String toString() {
+        return name
+    }
+
+    static constraints = {
+        name(unique: true)
+    }
+}
