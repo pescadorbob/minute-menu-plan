f5cf4a524bcf5115e191b3ed4fba6b1d46389227
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-04-09 05:56:25 +0000

    Added autoReconnect property

--- trunk/menuplanner/grails-app/conf/DataSource.groovy
+++ trunk/menuplanner/grails-app/conf/DataSource.groovy
@@ -4,6 +4,7 @@ dataSource {
     dialect = org.hibernate.dialect.MySQL5InnoDBDialect // must be set for transactions to work!
     username = "root"
     password = "igdefault"
+	validationQuery = "select 1"
 }
 hibernate {
     cache.use_second_level_cache=true
@@ -15,19 +16,19 @@ environments {
 	development {
 		dataSource {
 			dbCreate = "create" // one of 'create', 'create-drop','update'
-			url = "jdbc:mysql://localhost:3306/menuplanner"
+			url = "jdbc:mysql://localhost:3306/menuplanner?autoReconnect=true"
 		}
 	}
 	test {
 		dataSource {
 			dbCreate = "create-drop"
-			url = "jdbc:mysql://localhost:3306/menuplanner_test"
+			url = "jdbc:mysql://localhost:3306/menuplanner_test?autoReconnect=true"
 		}
 	}
 	production {
 		dataSource {
 			dbCreate = "update"
-			url = "jdbc:mysql://localhost:3306/menuplanner"
+			url = "jdbc:mysql://localhost:3306/menuplanner?autoReconnect=true"
 		}
 	}
 }
