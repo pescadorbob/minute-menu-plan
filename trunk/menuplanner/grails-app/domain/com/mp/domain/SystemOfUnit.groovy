d153ef5181237cac6502b7d497e9501e1527c6ef
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-04-15 07:27:55 +0000

    Added helper methods

--- trunk/menuplanner/grails-app/domain/com/mp/domain/SystemOfUnit.groovy
+++ trunk/menuplanner/grails-app/domain/com/mp/domain/SystemOfUnit.groovy
@@ -9,4 +9,12 @@ class SystemOfUnit {
     static constraints = {
         units(nullable: true)
     }
+
+    List<Unit> getTimeUnits(){
+        return ((units)? (units.findAll{it.metricType == MetricType.TIME} as List) : [])
+    }
+
+    List<Unit> getMetricUnits(){
+        return ((units)? (units.findAll{it.metricType == MetricType.METRIC} as List) : [])
+    }
 }
