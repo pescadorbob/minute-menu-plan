39ef45cda4534ea8da0159d11730aaceed4adb4a
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-04-15 20:47:23 +0000

    Fixed bug in pagination

--- trunk/menuplanner/grails-app/controllers/com/mp/domain/RecipeController.groovy
+++ trunk/menuplanner/grails-app/controllers/com/mp/domain/RecipeController.groovy
@@ -30,20 +30,30 @@ class RecipeController {
     }
 
     def list = {
-        Integer listSize=Recipe.count()
+        Integer listSize = Recipe.count()
         params.max = Math.min(params.max ? params.int('max') : 15, 150)
-        List<Category> categoryList=Category.list()
-        List<Recipe> recipeList= Recipe.list(params)
-        render(view:'list', model:[recipeList:recipeList,categoryList:categoryList, recipeTotal: Recipe.count()])
+        List<Category> categoryList = Category.list()
+        List<Recipe> recipeList = Recipe.list(params)
+        render(view: 'list', model: [recipeList: recipeList, categoryList: categoryList, recipeTotal: Recipe.count()])
     }
 
     def search = {
-        String query = params.query?:params.list("q")?.join(" ")
-        def search = Recipe.search([reload: true, max: 15, offset: params.offset?:0]) {
-            must(queryString(query))
+        List<Recipe> results = []
+        String query = params.query ?: params.list("q")?.join(" ")
+
+        Integer total
+        if (query && (query != 'null')) {
+            def search = Recipe.search([reload: true, max: 15, offset: params.offset ?: 0]) {
+                must(queryString(query))
+            }
+            results = search?.results
+            total = search?.total
+        } else {
+            results = Recipe.list(params)
+            total = Recipe.count()
         }
-        List<Recipe> results = search?.results
-        render(template:'/recipe/searchResultPanel',model:[recipeList:results, recipeTotal: search?.total, query: query])
+
+        render(template: '/recipe/searchResultPanel', model: [recipeList: results, recipeTotal: total, query: query])
     }
 
     def delete = {
@@ -75,19 +85,19 @@ class RecipeController {
         }
     }
 
-    def update={ RecipeCO recipeCO ->
-                if (recipeCO.validate()) {
-                    recipeCO.updateRecipe()
-                    redirect(action: 'show', id:recipeCO?.id)
-                } else {
-                    println recipeCO.errors.allErrors.each {
-                        println it
-                    }
-                    SystemOfUnit sys = SystemOfUnit.findBySystemName(SYSTEM_OF_UNIT_USA)
-                    List<Nutrient> nutrients = Nutrient.list()
-                    render(view: 'edit', model: [recipeCO: recipeCO, timeUnits: sys.timeUnits, metricUnits: sys.metricUnits, nutrients: nutrients])
-                }
+    def update = {RecipeCO recipeCO ->
+        if (recipeCO.validate()) {
+            recipeCO.updateRecipe()
+            redirect(action: 'show', id: recipeCO?.id)
+        } else {
+            println recipeCO.errors.allErrors.each {
+                println it
             }
+            SystemOfUnit sys = SystemOfUnit.findBySystemName(SYSTEM_OF_UNIT_USA)
+            List<Nutrient> nutrients = Nutrient.list()
+            render(view: 'edit', model: [recipeCO: recipeCO, timeUnits: sys.timeUnits, metricUnits: sys.metricUnits, nutrients: nutrients])
+        }
+    }
 
     def save = {RecipeCO recipeCO ->
         if (recipeCO.validate()) {
@@ -99,13 +109,13 @@ class RecipeController {
             }
             SystemOfUnit sys = SystemOfUnit.findBySystemName(SYSTEM_OF_UNIT_USA)
             List<Nutrient> nutrients = Nutrient.list()
-            List<Category> categories =[]
-            if(recipeCO?.categoryIds)
-                categories=Category.getAll(recipeCO?.categoryIds.flatten() as List)
+            List<Category> categories = []
+            if (recipeCO?.categoryIds)
+                categories = Category.getAll(recipeCO?.categoryIds.flatten() as List)
 //            List categoriesJson = categories.collect { [id: it.id, name: it.name] }
 //            println categoriesJson as JSON
 
-            render(view: 'create', model: [recipeCO: recipeCO, timeUnits: sys.timeUnits, metricUnits: sys.metricUnits, nutrients: nutrients, categories:categories])
+            render(view: 'create', model: [recipeCO: recipeCO, timeUnits: sys.timeUnits, metricUnits: sys.metricUnits, nutrients: nutrients, categories: categories])
         }
     }
 
@@ -142,19 +152,19 @@ class RecipeController {
         if (params.id) {
             image = Recipe.get(params.id.toLong()).image
         }
-        if(image){
-        byte[] fileContent = image.readFile()
-        String fileName = image.actualName + "." + image.extension
-        response.setContentLength(fileContent.size())
-        response.setHeader("Content-disposition", "attachment; filename=" + fileName)
-        response.setContentType("image/${image.extension}")
-        OutputStream out = response.getOutputStream()
-        out.write(fileContent)
-        out.flush()
-        out.close()
+        if (image) {
+            byte[] fileContent = image.readFile()
+            String fileName = image.actualName + "." + image.extension
+            response.setContentLength(fileContent.size())
+            response.setHeader("Content-disposition", "attachment; filename=" + fileName)
+            response.setContentType("image/${image.extension}")
+            OutputStream out = response.getOutputStream()
+            out.write(fileContent)
+            out.flush()
+            out.close()
         }
     }
- }
+}
 
 class RecipeCO {
     static config = ConfigurationHolder.config
@@ -191,7 +201,7 @@ class RecipeCO {
     def nutrientQuantities = []
 
     RecipeCO(Recipe recipe) {
-        id=recipe.id
+        id = recipe.id
         name = recipe.name
         difficulty = recipe.difficulty.name()
         shareWithCommunity = recipe.shareWithCommunity
@@ -219,10 +229,10 @@ class RecipeCO {
 
         nutrientIds = Nutrient.list()*.id
         Nutrient.count().times {
-            nutrientQuantities[it]=""
+            nutrientQuantities[it] = ""
         }
         recipe.nutrients.each {RecipeNutrient recipeNutrient ->
-            nutrientQuantities[recipeNutrient.nutrient.id.toInteger()-1] = recipeNutrient.quantity.value
+            nutrientQuantities[recipeNutrient.nutrient.id.toInteger() - 1] = recipeNutrient.quantity.value
         }
     } // parameterized constructor
 
@@ -234,7 +244,7 @@ class RecipeCO {
     }
 
     static constraints = {
-        id(nullable:true)
+        id(nullable: true)
         name(blank: false, matches: /[a-zA-Z0-9\s]*/)
 
         difficulty(blank: false, inList: RecipeDifficulty.list()*.name())
@@ -271,19 +281,19 @@ class RecipeCO {
         })
     }
 
-    public updateRecipe(){
+    public updateRecipe() {
         Recipe recipe = Recipe.get(id)
         recipe.name = name
         recipe.shareWithCommunity = shareWithCommunity
         recipe.servings = makesServing
         recipe.difficulty = RecipeDifficulty."${difficulty}"
 
-        recipe.preparationTime.unit=Unit.get(preparationUnitId)
+        recipe.preparationTime.unit = Unit.get(preparationUnitId)
         if (recipe.preparationTime.unit.name == TIME_UNIT_HOURS) preparationTime *= 60;
         recipe.preparationTime.value = preparationTime
 
 
-        recipe.cookingTime.unit=Unit.get(cookUnitId)
+        recipe.cookingTime.unit = Unit.get(cookUnitId)
         if (recipe.cookingTime.unit.name == TIME_UNIT_HOURS) cookTime *= 60;
         recipe.cookingTime.value = cookTime
 
@@ -308,16 +318,16 @@ class RecipeCO {
             Item item = Item.get(itemId)
             recipe.addToItems(item)
         }
-        
-        def temp1=recipe.directions
-        recipe.directions=[]
+
+        def temp1 = recipe.directions
+        recipe.directions = []
         temp1*.delete()
         directions.eachWithIndex {String step, Integer index ->
             new RecipeDirection(recipe: recipe, sequence: (index + 1), step: step).s()
         }
 
-        def temp2=recipe.ingredients
-        recipe.ingredients=[]
+        def temp2 = recipe.ingredients
+        recipe.ingredients = []
         temp2*.delete()
         ingredientQuantities.eachWithIndex {BigDecimal amount, Integer index ->
             MeasuredProduct product = MeasuredProduct.findByName(hiddenIngredientProductNames[index])
@@ -331,8 +341,8 @@ class RecipeCO {
             new RecipeIngredient(sequence: (index + 1), recipe: recipe, ingredient: product, quantity: quantity).s()
         }
 
-        def temp3=recipe.nutrients
-        recipe.nutrients=[]
+        def temp3 = recipe.nutrients
+        recipe.nutrients = []
         temp3*.delete()
         nutrientQuantities.eachWithIndex {def quantity, Integer index ->
             RecipeNutrient nutrient = new RecipeNutrient()
@@ -348,6 +358,7 @@ class RecipeCO {
             }
         }
     }
+
     public convertToRecipe() {
         Recipe recipe = new Recipe()
         recipe.name = name
--- trunk/menuplanner/grails-app/domain/com/mp/domain/Quantity.groovy
+++ trunk/menuplanner/grails-app/domain/com/mp/domain/Quantity.groovy
@@ -1,5 +1,7 @@
 package com.mp.domain
 
+import static com.mp.MenuConstants.*
+
 class Quantity {
 
     BigDecimal value
@@ -13,13 +15,14 @@ class Quantity {
         value(min: 0.0)
     }
 
-    //TODO: Implement unit conversion.
+    //TODO: Make it generic unit conversion method add(q1, q2).
 
-    public static Quantity add(Quantity q1, Quantity q2) {
+    public static Quantity addTime(Quantity q1, Quantity q2) {
         Quantity sum = new Quantity()
-        sum.value = q1.value + q2.value
-        sum.unit = q1.unit
+        sum.value = 0.00
+        sum.unit = Unit.findByName(TIME_UNIT_MINUTES)
+        sum.value += ((q1.unit.id == sum.unit.id) ? q1.value : (q1.value * 60))
+        sum.value += ((q2.unit.id == sum.unit.id) ? q2.value : (q2.value * 60))
         return sum
     }
-
 }
