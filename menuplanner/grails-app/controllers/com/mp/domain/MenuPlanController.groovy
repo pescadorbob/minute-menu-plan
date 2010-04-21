aee5d7aa4af9b9d1b84aaae45a5b573bd46a3454
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-04-21 11:16:06 +0000

    implemented search in menuPlan/show page.

--- menuplanner/grails-app/controllers/com/mp/domain/MenuPlanController.groovy
+++ menuplanner/grails-app/controllers/com/mp/domain/MenuPlanController.groovy
@@ -5,7 +5,33 @@ class MenuPlanController {
     def index = { }
 
     def show = {
+        Integer listSize = Recipe.count()
+        params.max = Math.min(params.max ? params.int('max') : 4, 150)
+        List<Category> categoryList = Category.list()
+        List<Recipe> recipeList = Recipe.list(params)
+
         MenuPlan menuPlan = MenuPlan.get(params.long("id"))
-        render(view: 'show', model: [weeks: menuPlan.weeks])
+        render(view: 'show', model: [weeks: menuPlan.weeks, categoryList:categoryList, recipeList:recipeList, recipeTotal: Recipe.count()])
     }
+
+    def search = {
+        List<Recipe> results = []
+        String query = params.query ?: params.list("q")?.join(" ")
+
+        Integer total
+        if (query && (query != 'null')) {
+            def search = Recipe.search([reload: true, max: 4, offset: params.offset ?: 0]) {
+                must(queryString(query))
+            }
+            results = search?.results
+            total = search?.total
+        } else {
+            params.max=4
+            results = Recipe.list(params)
+            total = Recipe.count()
+        }
+
+        render(template: '/menuPlan/searchResult', model: [recipeList: results, recipeTotal: total, query: query])
+    }
+    
 }
---
+++ menuplanner/grails-app/views/menuPlan/_searchResult.gsp
@@ -0,0 +1,49 @@
+<style type="text/css">
+.paginateButtons {
+    border: 0px;
+    color: #666;
+    font-size: 10px;
+    overflow: hidden;
+    padding: 10px 3px;
+    margin-top: 10px;
+}
+.paginateButtons a {
+    font-weight:bold;
+    font-size:11px;
+    color: royalblue;
+    margin: 0 3px;
+}
+.paginateButtons span {
+    padding: 2px 3px;
+}
+</style>
+
+<ul>
+    <g:each in="${recipeList}" var="recipe">
+        <g:render template="/menuPlan/recipeThumb" model="[recipe:recipe]"/>
+    </g:each>
+</ul>
+
+<div class="paginateButtons">
+    <util:remotePaginate
+            controller="menuPlan"
+            action="search"
+            total="${recipeTotal}"
+            params="[query: query]"
+            max="4"
+            offset="${params.offset}"
+            update="ratingbox"/>    
+</div>
+
+
+<script type="text/javascript">
+
+    jQuery(document).ready(function() {
+        jQuery('.recipe-detail-thumb').hover(function(){
+            jQuery(this).css('backgroundColor','orange')
+        }, function(){
+            jQuery(this).css('backgroundColor','#329ca7')
+        })
+    })
+
+</script>
