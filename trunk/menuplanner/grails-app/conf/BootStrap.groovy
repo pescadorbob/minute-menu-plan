8f2dba27817f83abb8bb21f799c847e35b675936
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-05-10 04:04:09 +0000

    Change in bootstrap

--- trunk/menuplanner/grails-app/conf/BootStrap.groovy
+++ trunk/menuplanner/grails-app/conf/BootStrap.groovy
@@ -68,7 +68,7 @@ class BootStrap {
             bootstrapService.populateMeasurableProduct()
             println "Populated Products"
             File recipeExcelFile
-            if(GrailsUtil.isDevelopmentEnv()){
+            if(!GrailsUtil.isDevelopmentEnv()){
                 recipeExcelFile=new File(ApplicationHolder.application.parentContext.servletContext.getRealPath("/bootstrapData/recipeSpreadsheet.xls"))
             } else{
                 recipeExcelFile=new File(ApplicationHolder.application.parentContext.servletContext.getRealPath("/bootstrapData/recipeSpreadsheet_Qa.xls"))
