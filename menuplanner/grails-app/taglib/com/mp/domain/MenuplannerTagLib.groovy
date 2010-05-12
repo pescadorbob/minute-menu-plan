eebd3b3e4a0e7bbdb14bf16d51e29118c220cdb5
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-05-12 13:34:41 +0000

    Removed tagInput plugin

--- menuplanner/grails-app/taglib/com/mp/domain/MenuplannerTagLib.groovy
+++ menuplanner/grails-app/taglib/com/mp/domain/MenuplannerTagLib.groovy
@@ -6,21 +6,6 @@ class MenuplannerTagLib {
 
     static namespace = 'mp'
 
-    def tagInput = {attrs ->
-        Map model = [:]
-        attrs.id = attrs.id ?: attrs.name
-        model.id = attrs.id
-        model.name = attrs.name
-        model.controller = attrs.controller
-        model.action = attrs.action
-        model.htmlClass = attrs.class ?: " "
-        model.prePopulated=attrs.prePopulated
-        if (attrs.multiselect == "false") {
-            model.tokenLimit = 1
-        }
-        out << g.render(template: '/tagInput/tagInput', model: model)
-    }
-
     def checkGeneralInfoTabError = {attrs ->
         def bean = attrs['bean']
         def fieldsToCheck = attrs['fields']
