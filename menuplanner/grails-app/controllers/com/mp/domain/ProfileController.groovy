66277a37c72f8f1cfab611de494d1d2a98520e0c
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-05-13 11:51:40 +0000

    worked on adding user.

--- menuplanner/grails-app/controllers/com/mp/domain/ProfileController.groovy
+++ menuplanner/grails-app/controllers/com/mp/domain/ProfileController.groovy
@@ -2,13 +2,23 @@ package com.mp.domain
 
 class ProfileController {
 
+    def manageProfileService
+
     def index = {
         render(view: 'create')
     }
     def create = {
         render(view: 'create')
     }
-    def save = {
-        render(view: 'create')
+    def save = {ProfileCO profileCO->
+        if (profileCO.validate()) {
+            User user = profileCO.convertToUser()
+            redirect(action: 'show', id: user?.id)
+        } else {
+            println profileCO.errors.allErrors.each {
+                println it
+            }
+            render(view: 'create', model: [profileCO: profileCO])
+        }
     }
 }
