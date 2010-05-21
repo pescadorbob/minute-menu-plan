013744aa3380c2c4dc1842fa2efb4ea84868cc90
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-05-21 15:47:51 +0000

    corrected saving password

--- trunk/menuplanner/grails-app/services/com/mp/domain/BootstrapService.groovy
+++ trunk/menuplanner/grails-app/services/com/mp/domain/BootstrapService.groovy
@@ -16,7 +16,7 @@ class BootstrapService {
             Integer intVal = ((new Random().nextInt(10) + 1) * 10)
             user.email = 'user' + index + '@gmail.com'
             user.name = 'user' + index
-            user.password = '1234'
+            user.password = '1234'.encodeAsBase64()
             user.city = 'city' + index
             user.mouthsToFeed = intVal
             List<UserType> roles = [UserType.User]
