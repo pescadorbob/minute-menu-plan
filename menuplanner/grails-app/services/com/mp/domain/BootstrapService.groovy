d30a13d154dcf902b89880ef225b3c6a60916b71
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-02 15:03:53 +0000

    changed text in comment on recipe.

--- menuplanner/grails-app/services/com/mp/domain/BootstrapService.groovy
+++ menuplanner/grails-app/services/com/mp/domain/BootstrapService.groovy
@@ -23,7 +23,7 @@ class BootstrapService {
             
             (1..new Random().nextInt(5)).each{       // comments on Recipe
                 user = User.get(new Random().nextInt(User.count()) + 1)
-                String commentText = 'Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum '
+                String commentText = "Recipe-${recipe.id} Comment-${it} Lorem ipsum  dolor sit amet, consectetur adipiscing elit. Donec ut sem felis, sed rhoncus purus. Donec mauris arcu, auctor sit amet tristique eget, egestas ut dui. Aenean quis eros sit amet tortor ullamcorper cursus ut nec urna. Proin scelerisque imperdiet lacus vel convallis. Morbi vehicula nisl eu mi tristique fringilla rhoncus sapien vulputate. " 
                 recipe?.addComment(user,commentText)
             }
             (1..new Random().nextInt(5)).each{      // add to Favorite
