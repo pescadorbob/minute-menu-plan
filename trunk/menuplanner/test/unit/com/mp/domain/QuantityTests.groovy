2a4fb55d11633304cd3013204deae48d3244f21a
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-15 13:50:40 +0000

    wrote a few unit test cases for add quantity

---
+++ trunk/menuplanner/test/unit/com/mp/domain/QuantityTests.groovy
@@ -0,0 +1,116 @@
+package com.mp.domain
+
+import grails.test.*
+import org.hibernate.classic.Session
+import org.apache.commons.lang.math.Fraction
+
+class QuantityTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+//
+
+  void test1_checkForNulls_add(){
+    def instances = []
+    mockDomain(Unit,instances)
+    Unit savedUnit = new Unit( name:'savedUnitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+    Unit unit = new Unit( name:'unitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+    Quantity quant1 = new Quantity(value:10.0,unit:unit,savedUnit:savedUnit)
+    Quantity quant2
+    Quantity quant = Quantity.add(quant1,quant2)
+    assertNotNull quant
+    assertEquals 10.0,quant.value
+
+    quant2= quant1
+    quant1=null
+
+    quant = Quantity.add(quant1,quant2)
+    assertNotNull quant
+    assertEquals 10.0,quant.value
+
+  }
+   void test1_valid_add(){
+    def instances = []
+    def unitInstances = []
+
+    mockDomain(SystemOfUnit,unitInstances)
+    mockDomain(Unit,instances)
+    mockDomain(StandardConversion)
+
+    Unit unit = new Unit( name:'unitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+    Unit savedUnit = new Unit( name:'savedUnitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+
+    Quantity quant1 = new Quantity(savedUnit:savedUnit)
+    Quantity quant2 = new Quantity(savedUnit:savedUnit)
+
+
+    def stanCon = mockFor(StandardConversion,true)
+    String str= "1 1 / 2"
+    stanCon.demand.static.getQuantityValueString(1..2) { Quantity quantity -> return str }
+    stanCon.demand.static.getQuantityToSave(1..2) {String a, Unit b -> return new Quantity(value:15.0,unit:unit,savedUnit:savedUnit) }
+
+    Quantity quant = Quantity.add(quant1,quant2)
+    assertNotNull quant
+    assertEquals 30,quant.value
+  }
+
+  void test1_Invalid_add(){
+    def instances = []
+    def unitInstances = []
+
+    mockDomain(SystemOfUnit,unitInstances)
+    mockDomain(Unit,instances)
+    mockDomain(StandardConversion)
+
+    Unit unit = new Unit( name:'unitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+    Unit savedUnit = new Unit( name:'savedUnitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+
+    Quantity quant1 = new Quantity(value:null,unit:null,savedUnit:savedUnit)
+    Quantity quant2 = new Quantity(value:null,unit:null,savedUnit:savedUnit)
+
+
+    def stanCon = mockFor(StandardConversion,true)
+    String str= "1 1 / 2"
+    stanCon.demand.static.getQuantityValueString(1..2) { Quantity quantity -> return null }
+    stanCon.demand.static.getQuantityToSave(1..2) {String a, Unit b -> return null }
+
+
+    Quantity quant = Quantity.add(quant1,quant2)
+    assertNull quant
+  }
+
+   //todo yet to be worked upon
+  void test2_valid_add(){
+//    def instances = []
+//    def unitInstances = []
+//
+//    mockDomain(SystemOfUnit,unitInstances)
+//    mockDomain(Unit,instances)
+//    mockDomain(StandardConversion)
+//
+//    Unit unit = new Unit( name:'unitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+//    Unit savedUnit = new Unit( name:'savedUnitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+//
+//    Quantity quant1 = new Quantity(savedUnit:savedUnit)
+//    Quantity quant2 = new Quantity(savedUnit:savedUnit)
+//
+//
+//    def stanCon = mockFor(StandardConversion,true)
+//    String str= "1 1 / 2"
+//    stanCon.demand.static.getQuantityValueString(1..2) { Quantity quantity -> return str }
+//    stanCon.demand.static.getQuantityToSave(1..2) {String a, Unit b -> return new Quantity(value:15.0,unit:unit,savedUnit:savedUnit) }
+//
+//    Quantity quant = Quantity.add(quant1,quant2)
+//    assertNotNull quant
+//    assertEquals 30,quant.value
+  }
+
+
+
+
+}
---
+++ trunk/menuplanner/test/unit/com/mp/domain/StandardConversionTests.groovy
@@ -0,0 +1,23 @@
+package com.mp.domain
+
+import grails.test.*
+
+class StandardConversionTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+   // todo needs to be worked upon 
+    void test_getQuantityToSave() {
+//      //String amountFraction, Unit displayUnit
+//
+//    mockDomain(Unit,instances)
+//    Unit unit = new Unit( name:'unitname',symbol:'S',definition:'def', metricType:MetricType.UNIT)
+//    StandardConversion sc = new StandardConversion()
+//
+
+    }
+}
