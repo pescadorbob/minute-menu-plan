0dd8abe9f661cdf544d8d3a1d8511f2a5c58173b
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-15 15:26:18 +0000

    added functional test cases for admin menu

---
+++ menuplanner/src/groovy/com/mp/domain/TestConstants.groovy
@@ -0,0 +1,11 @@
+package com.mp.domain
+
+class TestConstants {
+    public static final String SUPER_ADMIN = "qa.menuplanner+superAdmin@gmail.com"
+    public static final String ADMIN_1 = "qa.menuplanner+admin1@gmail.com"
+    public static final String ADMIN_2 = "qa.menuplanner+admin2@gmail.com"
+    public static final String USER_1 = "qa.menuplanner+user1@gmail.com"
+    public static final String USER_2 = "qa.menuplanner+user2@gmail.com"
+    public static final String USER_PASSWORD = "1234"
+
+}
