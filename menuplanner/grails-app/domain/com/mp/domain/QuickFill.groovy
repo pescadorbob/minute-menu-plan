9e8e7b8351c31a38fd22737f94b7160e54207be0
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-18 10:34:55 +0000

    Added helper methods in QuickFill domain class

--- menuplanner/grails-app/domain/com/mp/domain/QuickFill.groovy
+++ menuplanner/grails-app/domain/com/mp/domain/QuickFill.groovy
@@ -7,6 +7,37 @@ class QuickFill {
 
     static hasMany = [mealItems: Meal]
 
+    static transients = ['week1', 'week2', 'week3', 'week4', 'breakfast', 'lunch']
+
+    Meal getWeek1(){
+        return getDinnerForWeek(1)
+    }
+
+    Meal getWeek2(){
+        return getDinnerForWeek(2)
+    }
+
+    Meal getWeek3(){
+        return getDinnerForWeek(3)
+    }
+
+    Meal getWeek4(){
+        return getDinnerForWeek(4)
+    }
+
+    Meal getDinnerForWeek(Integer week){
+        List<Meal> dinnerMeals = mealItems.findAll{it.type == MealType.DINNER}
+        return dinnerMeals[(week-1)]
+    }
+
+    Meal getBreakfast(){
+        return mealItems.find{it.type == MealType.BREAKFAST}
+    }
+
+    Meal getLunch(){
+        return mealItems.find{it.type == MealType.LUNCH}
+    }
+
     static constraints = {
         name(unique: true)
     }
