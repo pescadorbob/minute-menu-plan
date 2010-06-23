cad3ffb6a0d5311f4934b783999d66e58b8a83c9
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-23 10:40:46 +0000

    small refactoring in shopping list create/update

--- trunk/menuplanner/grails-app/controllers/com/mp/domain/ShoppingListController.groovy
+++ trunk/menuplanner/grails-app/controllers/com/mp/domain/ShoppingListController.groovy
@@ -7,54 +7,39 @@ class ShoppingListController {
 
     def index = { }
 
-    WeeklyShoppingList createWeeklyShoppingListFromMenuPlan(List<String> groceries, ShoppingList shoppingList, MenuPlan menuPlan, Integer index) {
-        WeeklyShoppingList weeklyShoppingList = new WeeklyShoppingList()
-        weeklyShoppingList.weekIndex = index
-        shoppingList.addToWeeklyShoppingLists(weeklyShoppingList)
-        weeklyShoppingList.s()
-        Week week = menuPlan?.weeks?.get(index)
-        week.days.each {Day day ->
-            List<Item> items = day?.breakfast + day?.lunch + day?.dinner
-            items.each {Item item ->
-                if (item?.instanceOf(Recipe)) {
-                    item?.ingredients?.each {RecipeIngredient ingredient ->
-                        if (ingredient?.ingredient?.id in weeklyShoppingList?.products*.item?.id) {
-                            def shoppingIngredient = weeklyShoppingList?.products?.find {it?.item?.id == ingredient?.ingredient?.id}
-                            Quantity quantity = Quantity.add(shoppingIngredient?.quantity, ingredient?.quantity)
-                            quantity.s()
-                            shoppingIngredient?.quantity = quantity
-                            shoppingIngredient.s()
-                        } else {
-                            ShoppingIngredient shoppingIngredient = new ShoppingIngredient()
-                            shoppingIngredient.item = ingredient.ingredient
-                            shoppingIngredient.quantity = ingredient.quantity
-                            shoppingIngredient.weeklyShoppingList = weeklyShoppingList
-                            weeklyShoppingList.addToProducts(shoppingIngredient)
-                            shoppingIngredient.s()
-                        }
-                    }
-                }
-            }
+    def generateShoppingList = {
+        MenuPlan menuPlan = MenuPlan.get(params?.id?.toLong())
+        User user = User.currentUser
+        ShoppingList shoppingList = ShoppingList?.findByMenuPlanAndUser(menuPlan, user)
+        if (!shoppingList) {
+            PrintShoppingListCO pslCO = new PrintShoppingListCO()
+            pslCO.name = menuPlan?.name + '-Shopping List'
+            pslCO.menuPlanId = params?.id
+            pslCO.weeks = ['0', '1', '2', '3']
+            pslCO.servings = user.mouthsToFeed.toString()
+            List<MenuPlan> menuPlans = user?.menuPlans as List
+            render(view: 'generateShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, servings: user.mouthsToFeed])
+        } else {
+            redirect(controller: 'shoppingList', action: 'show', id: shoppingList?.id)
         }
-        groceries?.each {String name ->
-            Item item = Item.findByName(name)
-            if (!item && name.trim()) {
-                item = new Product(name: name).s()
-            }
-            if (!(item?.id in weeklyShoppingList?.groceries*.id)) {
-                weeklyShoppingList.groceries.add(item)
-            }
-        }
-        return weeklyShoppingList
     }
 
-    void createWeeklyShoppingList(List<ShoppingIngredient> groceries, List<ShoppingIngredient> products, ShoppingList shoppingList, Integer index) {
-        WeeklyShoppingList weeklyShoppingList = new WeeklyShoppingList()
-        weeklyShoppingList.weekIndex = index
-        shoppingList.addToWeeklyShoppingLists(weeklyShoppingList)
-        weeklyShoppingList.products = products
-        weeklyShoppingList.groceries = groceries
-        weeklyShoppingList.s()
+    def create = {PrintShoppingListCO pslCO ->
+        if (pslCO.validate()) {
+            ShoppingList shoppingList = shoppingListService.createShoppingList(pslCO)
+            render(view: 'create', model: [shoppingList: shoppingList])
+        } else {
+            pslCO.errors.allErrors.each {
+                println it
+            }
+            User user = User.currentUser
+            List<MenuPlan> menuPlans = user?.menuPlans as List
+            render(view: 'generateShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, servings: user.mouthsToFeed])
+        }
+    }
+    def show = {
+        ShoppingList shoppingList = ShoppingList.get(params.id)
+        render(view: 'show', model: [shoppingList: shoppingList])
     }
 
     def save = {
@@ -106,51 +91,16 @@ class ShoppingListController {
             }
             weeklyShoppingList.products = products
             weeklyShoppingList.groceries = groceries
-
             shoppingList.addToWeeklyShoppingLists(weeklyShoppingList)
             weeklyShoppingList.s()
         }
         redirect(controller: 'shoppingList', action: 'show', id: shoppingList?.id)
     }
 
-    def show = {
-        ShoppingList shoppingList = ShoppingList.get(params.id)
-        render(view: 'show', model: [shoppingList: shoppingList])
-    }
 
-    def printShoppingList = {
-        MenuPlan menuPlan = MenuPlan.get(params?.id?.toLong())
-        User user = User.currentUser
-        ShoppingList shoppingList = ShoppingList?.findByMenuPlanAndUser(menuPlan, user)
-        if (!shoppingList) {
-            PrintShoppingListCO pslCO = new PrintShoppingListCO()
-            pslCO.name = menuPlan?.name + '-Shopping List'
-            pslCO.menuPlanId = params?.id
-            pslCO.weeks = ['0', '1', '2', '3']
-            pslCO.servings = user.mouthsToFeed.toString()
-            List<MenuPlan> menuPlans = user?.menuPlans as List
-            render(view: 'printShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, servings: user.mouthsToFeed])
-        } else {
-            redirect(controller: 'shoppingList', action: 'show', id: shoppingList?.id)
-        }
-    }
-
-    def create = {PrintShoppingListCO pslCO ->
-        if (pslCO.validate()) {
-            ShoppingList shoppingList = shoppingListService.createShoppingList(pslCO)
-            render(view: 'create', model: [shoppingList: shoppingList])
-        } else {
-            pslCO.errors.allErrors.each {
-                println it
-            }
-            User user = User.currentUser
-            List<MenuPlan> menuPlans = user?.menuPlans as List
-            render(view: 'printShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, servings: user.mouthsToFeed])
-        }
-    }
 
     def cancelDetailShoppingList = {
-        redirect(controller: 'shoppingList', action: 'printShoppingList', id: params?.menuPlanId)
+        redirect(controller: 'shoppingList', action: 'generateShoppingList', id: params?.menuPlanId)
     }
 
     def edit = {
@@ -162,7 +112,7 @@ class ShoppingListController {
         pslCO.servings = shoppingList?.servings
         pslCO.weeks = shoppingList?.weeklyShoppingLists*.weekIndex*.toString()
         List<MenuPlan> menuPlans = user?.menuPlans as List
-        render(view: 'printShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, shoppingListId: shoppingList?.id, servings: user.mouthsToFeed, shoppingList: shoppingList])
+        render(view: 'generateShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, shoppingListId: shoppingList?.id, servings: user.mouthsToFeed, shoppingList: shoppingList])
     }
 
     def modifyShoppingList = {PrintShoppingListCO pslCO ->
@@ -184,29 +134,13 @@ class ShoppingListController {
             }
             User user = User.currentUser
             List<MenuPlan> menuPlans = user?.menuPlans as List
-            render(view: 'printShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, servings: user.mouthsToFeed])
+            render(view: 'generateShoppingList', model: [pslCO: pslCO, menuPlans: menuPlans, servings: user.mouthsToFeed])
         }
-    }
-
-    def printerFriendlyShoppingList = {
-        ShoppingList shoppingList = ShoppingList.get(params.id)
-        render(view: 'printerFriendlyShoppingList', model: [shoppingList: shoppingList])
-    }
-
-    def emailShoppingList = {
-        ShoppingList shoppingList = ShoppingList.get(params.id)
-        asynchronousMailService.sendAsynchronousMail {
-            to User.currentUser?.email
-            subject "Your Shopping List : ${shoppingList.name}"
-            html g.render(template: '/shoppingList/emailShoppingList', model: [shoppingList: shoppingList])
-        }
-        render "Email sent to ${User.currentUser?.email}"
     }
 
     def update = {
         List<String> weekList = params.weekList?.tokenize("[], ")
         ShoppingList shoppingList = ShoppingList.get(params?.shoppingListId?.toLong())
-        MenuPlan menuPlan = shoppingList?.menuPlan
 
         List<WeeklyShoppingList> weeklyShoppingLists = shoppingList.weeklyShoppingLists
         shoppingList.weeklyShoppingLists = []
@@ -223,7 +157,6 @@ class ShoppingListController {
 
             List<ShoppingIngredient> products = []
             List<ShoppingIngredient> groceries = []
-
             Map groceriesByWeek = params.findAll {it.key.contains("week${index}.groceries")}
             groceriesByWeek.each {key, value ->
                 String aisleId = key.tokenize(".")?.last()
@@ -240,7 +173,6 @@ class ShoppingListController {
                     groceries.add(shoppingIngredient.s())
                 }
             }
-
             Map productsByWeek = params.findAll {it.key.contains("week${index}.products")}
             productsByWeek.each {key, value ->
                 String aisleId = key.tokenize(".")?.last()
@@ -264,5 +196,21 @@ class ShoppingListController {
         }
         redirect(controller: 'shoppingList', action: 'show', id: shoppingList?.id)
     }
-    
+
+
+    def printerFriendlyShoppingList = {
+        ShoppingList shoppingList = ShoppingList.get(params.id)
+        render(view: 'printerFriendlyShoppingList', model: [shoppingList: shoppingList])
+    }
+
+    def emailShoppingList = {
+        ShoppingList shoppingList = ShoppingList.get(params.id)
+        asynchronousMailService.sendAsynchronousMail {
+            to User.currentUser?.email
+            subject "Your Shopping List : ${shoppingList.name}"
+            html g.render(template: '/shoppingList/emailShoppingList', model: [shoppingList: shoppingList])
+        }
+        render "Email sent to ${User.currentUser?.email}"
+    }
+
 }
