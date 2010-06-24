780f5ac812dfd4b86ef8319bf29daa0e3927dda9
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-24 19:30:42 +0000

    Re#58 Implemented domain class PreparationMethod, updated excelsheets and bootstap, saving preparation method in RecipeIngredient

--- trunk/menuplanner/grails-app/domain/com/mp/domain/RecipeIngredient.groovy
+++ trunk/menuplanner/grails-app/domain/com/mp/domain/RecipeIngredient.groovy
@@ -8,16 +8,18 @@ class RecipeIngredient {
     Item ingredient
     Quantity quantity
     Aisle aisle
+    PreparationMethod preparationMethod
 
     static belongsTo = [recipe: Recipe]
 
     String toString() {
-        return ("${quantity ? quantity : ''} ${ingredient}")
+        return ("${quantity ? quantity : ''} ${ingredient}${preparationMethod ? ' ('+preparationMethod+')' : ''}")
     }
 
     static constraints = {
         ingredient(unique: 'recipe')
         quantity(nullable: true, blank: true)
+        preparationMethod(nullable: true, blank: true)
         aisle(nullable: true, blank: true)
     }
 
