00a111dd429bec133978f311765e6afe5735eff3
chandan <chandan@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-25 10:38:35 +0000

    Removed a  CO class from ShoppingListService

--- menuplanner/grails-app/services/com/mp/domain/ShoppingListService.groovy
+++ menuplanner/grails-app/services/com/mp/domain/ShoppingListService.groovy
@@ -85,20 +85,3 @@ class ShoppingListService {
 //    }
 }
 
-class PrintShoppingListCO {
-    String name
-    String menuPlanId
-    List<String> weeks
-    String servings
-
-    static constraints = {
-        name(blank: false, nullable: false)
-        weeks(validator: {obj, val ->
-            if (!val) {
-                return 'default.blank.message'
-            }
-        })
-        menuPlanId(nullable: false, blank: false)
-        servings(blank: false, matches: /[0-9\s]*/)
-    }
-}
