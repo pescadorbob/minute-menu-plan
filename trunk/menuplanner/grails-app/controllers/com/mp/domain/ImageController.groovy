ab0073653860cf47158e19465e3cc91096e8c22a
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-28 08:31:36 +0000

    Fixed #74 Showing products without images in one line

--- trunk/menuplanner/grails-app/controllers/com/mp/domain/ImageController.groovy
+++ trunk/menuplanner/grails-app/controllers/com/mp/domain/ImageController.groovy
@@ -5,29 +5,33 @@ import org.codehaus.groovy.grails.commons.ApplicationHolder
 
 class ImageController {
 
-def grailsApplication
+    def grailsApplication
     def static config = ConfigurationHolder.config
 
     def image = {
         Image image
-        if(params?.id){
+        if (params?.id) {
             image = Image.get(params.id)
         }
         byte[] fileContent
         String extension = image?.extension
         if (image) {
             fileContent = image.readFile()
-        } else{
-            File noImageFile =new File(ApplicationHolder.application.parentContext.servletContext.getRealPath("/images/${params?.noImage}"))
-            fileContent = noImageFile.readBytes()
-            extension = noImageFile?.name?.tokenize('.')?.tail()?.join('.')
+        } else {
+            File noImageFile = new File(ApplicationHolder.application.parentContext.servletContext.getRealPath("/images/${params?.noImage}"))
+            if (noImageFile.exists()) {
+                fileContent = noImageFile.readBytes()
+                extension = noImageFile?.name?.tokenize('.')?.tail()?.join('.')
+            }
         }
-        response.setContentLength(fileContent.size())
-        response.setContentType("image/${extension}")
-        OutputStream out = response.getOutputStream()
-        out.write(fileContent)
-        out.flush()
-        out.close()
+        if (fileContent) {
+            response.setContentLength(fileContent.size())
+            response.setContentType("image/${extension}")
+            OutputStream out = response.getOutputStream()
+            out.write(fileContent)
+            out.flush()
+            out.close()
+        }
     }
     def imageByPath = {
         File imageFile
@@ -37,12 +41,14 @@ def grailsApplication
         if (!imageFile) {
             imageFile = new File(ApplicationHolder.application.parentContext.servletContext.getRealPath("/images/${params.noImage}"))
         }
-        byte[] fileContent = imageFile?.readBytes()
-        response.setContentLength(fileContent.size())
-        response.setContentType("image/${imageFile.name.tokenize('.').tail().join('.')}")
-        OutputStream out = response.getOutputStream()
-        out.write(fileContent)
-        out.flush()
-        out.close()
+        if (imageFile.exists()) {
+            byte[] fileContent = imageFile?.readBytes()
+            response.setContentLength(fileContent.size())
+            response.setContentType("image/${imageFile.name.tokenize('.').tail().join('.')}")
+            OutputStream out = response.getOutputStream()
+            out.write(fileContent)
+            out.flush()
+            out.close()
+        }
     }
 }
