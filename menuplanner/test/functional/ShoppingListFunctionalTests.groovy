a65a85980233896495dc3fbeb9d07b5c54e40a07
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-06-29 10:55:01 +0000

    fixed functional test cases for shopping list

--- menuplanner/test/functional/ShoppingListFunctionalTests.groovy
+++ menuplanner/test/functional/ShoppingListFunctionalTests.groovy
@@ -3,81 +3,110 @@ import com.mp.domain.*
 class ShoppingListFunctionalTests extends MenuPlannerFunctionalTests {
     void testSomething() {}
 
-//    void testShoppingListPage() {
-//        goToShoppingListPage()
-//        assertContentContains "Shopping List of Parameters"
-//        assertContentContains "Name of shopping list"
-//        assertContentContains "Choose MenuPlan "
-//    }
-//
-//    void testGenerateNewShoppingList_Valid_Values() {
-//        goToShoppingListPage()
-//        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
-//        createShoppingList(shoppingListFormData)
-//        assertContentContains shoppingListFormData.name
-//        assertContentContains "Export to Todo"
-//        assertContentContains "Print Shopping List "
-//    }
-//
-//    void testGenerateNewShoppingList_One_Week_Selected() {
-//        goToShoppingListPage()
-//        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
-//        createShoppingList_One_Week_Selected(shoppingListFormData)
-//        assertContentContains shoppingListFormData.name
-//        assertContentContains "Export to Todo"
-//        assertContentContains "Print Shopping List "
-//    }
-//
-//    void testGenerateNewShoppingList_All_Blank_Fields() {
-//        goToShoppingListPage()
-//        createShoppingList_All_Fields_Blank()
-//        assertTrue(
-//                (byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.servings.blank.servings'))) &&
-//                        (byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.name.blank.error.com.mp.domain.PrintShoppingListCO.name')))
-//        )
-//
-//    }
-//
-//    void testGenerateNewShoppingList_Blank_Name() {
-//        goToShoppingListPage()
-//        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
-//        shoppingListFormData.name = ""
-//        createShoppingList(shoppingListFormData)
-//        assertTrue(byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.name.blank.error.com.mp.domain.PrintShoppingListCO.name')))
-//    }
-//
-//    void testGenerateNewShoppingList_Blank_Weeks() {
-//        goToShoppingListPage()
-//        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
-//        createShoppingList_Blank_Weeks(shoppingListFormData)
+    void testCreateShoppingListPage() {
+        goToCreateShoppingListPage()
+        assertContentContains "Shopping List of Parameters"
+        assertContentContains "Name of shopping list"
+        assertContentContains "Choose MenuPlan"
+        assertContentContains "Servings"
+        assertContentContains "Week1"
+        assertContentContains "Week2"
+        assertContentContains "Week3"
+        assertContentContains "Week4"
+    }
+
+    void testGenerateNewShoppingList_Valid_Values() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        createShoppingList(shoppingListFormData)
+        assertContentContains shoppingListFormData.name
+        assertContentContains "Week 1"
+        assertContentContains "Week 2"
+        assertContentContains "Week 3"
+        assertContentContains "Week 4"
+        assertContentContains "Add items to Shopping List"
+    }
+
+    void testGenerateNewShoppingList_Three_Weeks_Selected() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        createShoppingList_Three_Weeks_Selected(shoppingListFormData)
+        assertContentContains shoppingListFormData.name
+        assertContentContains "Week 2"
+        assertContentContains "Week 3"
+        assertContentContains "Week 4"
+        assertContentContains "Add items to Shopping List"
+    }
+
+    void testGenerateNewShoppingList_All_Blank_Fields() {
+        goToCreateShoppingListPage()
+        createShoppingList_All_Fields_Blank()
+        assertTrue(
+                (byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.servings.blank.servings'))) &&
+                        (byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.name.blank.error.com.mp.domain.PrintShoppingListCO.name')))
+        )
+
+    }
+
+    void testGenerateNewShoppingList_Blank_Name() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        shoppingListFormData.name = ""
+        createShoppingList(shoppingListFormData)
+        assertTrue(byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.name.blank.error.com.mp.domain.PrintShoppingListCO.name')))
+    }
+
+    void testGenerateNewShoppingList_Blank_Weeks() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        createShoppingList_Blank_Weeks(shoppingListFormData)
+        //TODO: Implement blank validation on weeks
 //        assertTrue(byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.weeks.nullable.error.weeks')))
-//    }
-//
-//    void testGenerateNewShoppingList_Blank_Servings() {
-//        goToShoppingListPage()
-//        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
-//        shoppingListFormData.servings = ""
-//        createShoppingList(shoppingListFormData)
-//        assertTrue(byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.servings.blank.servings')))
-//    }
-//
-//    void testGenerateNewShoppingList_Invalid_Servings_Value() {
-//        goToShoppingListPage()
-//        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
-//        shoppingListFormData.servings = "abcd"
-//        createShoppingList(shoppingListFormData)
-//        assertTrue(byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.servings.matches.error.servings')))
-//    }
-//
-//    void testGenerateShoppingList_AddItem() {
-//        goToShoppingListPage()
-//        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
-//        createShoppingList(shoppingListFormData)
-//        assertContentContains shoppingListFormData.name
-//        assertContentContains "Export to Todo"
-//        assertContentContains "Print Shopping List "
-//        byId('addItemTxt_0').setValue("Functional Test Item")
-//        byId('addItemBtn_0').click()
-//        assertElementTextContains 'groceries_0', 'Functional Test Item'
-//    }
+    }
+
+    void testGenerateNewShoppingList_Blank_Servings() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        shoppingListFormData.servings = ""
+        createShoppingList(shoppingListFormData)
+        assertTrue(byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.servings.blank.servings')))
+    }
+
+    void testGenerateNewShoppingList_Invalid_Servings_Value() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        shoppingListFormData.servings = "abcd"
+        createShoppingList(shoppingListFormData)
+        assertTrue(byId('errorsDiv').asText().contains(getMessage('printShoppingListCO.servings.matches.error.servings')))
+    }
+
+    void testGenerateShoppingList_AddItem() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        createShoppingList(shoppingListFormData)
+        assertContentContains shoppingListFormData.name
+        byId('aisleList_0').getOption(6).getValueAttribute()
+        byId('addItemTxt_0').setValue("Functional Test Item")
+        byId('addItemBtn_0').click()
+    }
+
+    void testCreateShoppingList() {
+        goToCreateShoppingListPage()
+        ShoppingListFormData shoppingListFormData = ShoppingListFormData.getDefaultShoppingListFormData()
+        createShoppingList(shoppingListFormData)
+        assertContentContains shoppingListFormData.name
+        assertContentContains "Add items to Shopping List"
+        def saveShoppingListButton = byName('_action_save')
+        saveShoppingListButton.click()
+        get('/shoppingList/show/1')
+        assertContentContains "Week 1"
+        assertContentContains "Week 2"
+        assertContentContains "Week 3"
+        assertContentContains "Week 4"
+        assertContentContains "snacks"
+        assertContentContains "condiments"
+        assertContentContains "canned foods"
+        assertContentContains "Print Shopping List"
+        assertContentContains "Email the Shopping List"
+    }
 }
