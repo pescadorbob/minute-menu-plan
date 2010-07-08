1675186729dcf7cdfdb7f13abc00a25309714e0d
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-07-08 11:31:13 +0000

    Fixed#61 Using recipe ingredients quantity based on number of servings in shopping list

--- trunk/menuplanner/grails-app/services/com/mp/domain/ShoppingListService.groovy
+++ trunk/menuplanner/grails-app/services/com/mp/domain/ShoppingListService.groovy
@@ -41,12 +41,12 @@ class ShoppingListService {
     WeeklyShoppingList createWeeklyShoppingList(ShoppingList shoppingList, MenuPlan menuPlan, String index) {
         WeeklyShoppingList weeklyShoppingList = new WeeklyShoppingList()
         weeklyShoppingList.weekIndex = index.toInteger()
-        weeklyShoppingList.products = getProductListForWeekFromMenuPlan(menuPlan, index)
+        weeklyShoppingList.products = getProductListForWeekFromMenuPlan(menuPlan, index, shoppingList)
         weeklyShoppingList.groceries = getGroceryListForWeekFromMenuPlan(menuPlan, index)
         return weeklyShoppingList
     }
 
-    List<ShoppingIngredient> getProductListForWeekFromMenuPlan(MenuPlan menuPlan, String weekIndex) {
+    List<ShoppingIngredient> getProductListForWeekFromMenuPlan(MenuPlan menuPlan, String weekIndex, ShoppingList shoppingList) {
         List<ShoppingIngredient> productListForWeek = []
         List<RecipeIngredient> weeklyRecipeIngredients = []
         Week week = menuPlan?.weeks[weekIndex?.toInteger()]
@@ -55,7 +55,7 @@ class ShoppingListService {
             day.meals.each {Meal meal ->
                 meal.items.each {Item item ->
                     if (item?.instanceOf(Recipe.class)) {
-                        weeklyRecipeIngredients += getRecipeIngredientsList(item)
+                        weeklyRecipeIngredients += getRecipeIngredientsList(item, shoppingList)
                     }
                 }
             }
@@ -75,7 +75,7 @@ class ShoppingListService {
                 similarIngredients.each {RecipeIngredient similarIngredient ->
                     total = (total == null) ? similarIngredient.quantity : Quantity.add(total, similarIngredient.quantity)
                 }
-                ShoppingIngredient shoppingIngredient = new ShoppingIngredient(name: "${(total)? total.toBiggestUnitString() + ' ': ''}" + differentIngredient.name, aisle: (aisle?.id) ? aisle : null)
+                ShoppingIngredient shoppingIngredient = new ShoppingIngredient(name: "${(total) ? total.toBiggestUnitString() + ' ' : ''}" + differentIngredient.name, aisle: (aisle?.id) ? aisle : null)
                 productListForWeek.add(shoppingIngredient)
             }
         }
@@ -83,32 +83,61 @@ class ShoppingListService {
         return productListForWeek
     }
 
-    List<RecipeIngredient> getRecipeIngredientsList(def recipe) {
+    List<RecipeIngredient> getRecipeIngredientsList(def recipe, ShoppingList shoppingList) {
         List<RecipeIngredient> ingredients = []
+
         if (recipe?.instanceOf(Recipe.class)) {
             recipe.ingredients.each {RecipeIngredient recipeIngredient ->
-                if (recipeIngredient.ingredient?.instanceOf(Recipe.class)) {
-                    List<RecipeIngredient> subRecipeIngredients = getRecipeIngredientsList(recipeIngredient.ingredient as Recipe)
-                    subRecipeIngredients.each {RecipeIngredient subRecipeIngredient ->
-                        if (subRecipeIngredient.quantity && recipeIngredient.quantity) {
-                            RecipeIngredient newSubRecipeIngredient = new RecipeIngredient()
-                            newSubRecipeIngredient.with {
-                                ingredient = subRecipeIngredient.ingredient
-                                quantity = Quantity.multiply(subRecipeIngredient.quantity, recipeIngredient.quantity)
-                                aisle = subRecipeIngredient.aisle
-                                preparationMethod = subRecipeIngredient.preparationMethod
-                            }
-                            if (newSubRecipeIngredient.quantity) {ingredients.add(newSubRecipeIngredient)}
-                        } else {
-                            if (subRecipeIngredient.quantity) {ingredients.add(subRecipeIngredient)}
-                        }
-                    }
-                } else {
-                    if (recipeIngredient.quantity) {ingredients.add(recipeIngredient)}
+
+//                if (recipeIngredient.ingredient?.instanceOf(Recipe.class)) {
+//                    List<RecipeIngredient> subRecipeIngredients = getRecipeIngredientsList(recipeIngredient.ingredient as Recipe, shoppingList)
+//                    subRecipeIngredients.each {RecipeIngredient subRecipeIngredient ->
+//                        if (subRecipeIngredient.quantity && recipeIngredient.quantity) {
+//                            RecipeIngredient newSubRecipeIngredient = new RecipeIngredient()
+//                            newSubRecipeIngredient.with {
+//                                ingredient = subRecipeIngredient.ingredient
+//                                quantity = Quantity.multiply(subRecipeIngredient.quantity, recipeIngredient.quantity)
+//                                aisle = subRecipeIngredient.aisle
+//                                preparationMethod = subRecipeIngredient.preparationMethod
+//                            }
+//                            if (newSubRecipeIngredient.quantity) {ingredients.add(newSubRecipeIngredient)}
+//                        } else {
+//                            if (subRecipeIngredient.quantity) {
+//                                RecipeIngredient recipeIngredientNew = cloneRecipeIngredient(subRecipeIngredient)
+//                                recipeIngredientNew.quantity.value = ((recipeIngredient.quantity.value * shoppingList.servings) / recipeIngredient.recipe.servings).toFloat()
+//                                ingredients.add(recipeIngredientNew)
+//                            }
+//                        }
+//                    }
+
+//                    RecipeIngredient recipeIngredientNew = cloneRecipeIngredient(recipeIngredient)
+//                    recipeIngredientNew.quantity.value = ((recipeIngredient.quantity.value * shoppingList.servings) / recipeIngredient.recipe.servings).toFloat()
+//                    ingredients.add(recipeIngredientNew)
+//                } else {
+                if (recipeIngredient.quantity) {
+                    RecipeIngredient recipeIngredientNew = cloneRecipeIngredient(recipeIngredient)
+                    Float value = (recipeIngredient?.quantity?.value) ? recipeIngredient.quantity.value : 1.0f
+                    Integer servings = recipeIngredient.recipe.servings
+                    recipeIngredientNew.quantity.value = ((value * shoppingList.servings) / servings).toFloat()
+                    ingredients.add(recipeIngredientNew)
                 }
+//                }
             }
         }
         return ingredients
+    }
+
+    RecipeIngredient cloneRecipeIngredient(RecipeIngredient recipeIngredient) {
+        RecipeIngredient recipeIngredientNew = new RecipeIngredient()
+        Quantity quantity = new Quantity()
+        quantity.unit = recipeIngredient.quantity.unit
+        quantity.savedUnit = recipeIngredient.quantity.savedUnit
+
+        recipeIngredientNew.ingredient = recipeIngredient.ingredient
+        recipeIngredientNew.quantity = quantity
+        recipeIngredientNew.aisle = recipeIngredient.aisle
+        recipeIngredientNew.preparationMethod = recipeIngredient.preparationMethod
+        return recipeIngredientNew
     }
 
     List<ShoppingIngredient> getGroceryListForWeekFromMenuPlan(MenuPlan menuPlan, String weekIndex) {
