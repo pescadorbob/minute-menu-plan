033e8f9e48347091c4e9075b35fb14fb44dab2fc
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-09-27 13:22:02 +0000

    small change

--- trunk/menuplanner/test/functional/MenuPlannerFunctionalTests.groovy
+++ trunk/menuplanner/test/functional/MenuPlannerFunctionalTests.groovy
@@ -90,7 +90,6 @@ class MenuPlannerFunctionalTests extends functionaltestplugin.FunctionalTestCase
             password = userFormData.password
             confirmPassword = userFormData.confirmPassword
             name = userFormData.name
-            if (!userFormData.isEnabled) { byId('chk_Enable').click() }
             if (userFormData.isSuperAdmin) {byId('chk_SuperAdmin').click()}
             if (userFormData.isAdmin) { byId('chk_Admin').click()}
             if (userFormData.isUser) {byId('chk_Subscriber').click()}
