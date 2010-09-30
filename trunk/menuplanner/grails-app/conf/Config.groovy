40bc8d5b7b47e817befc03d36f17c95639daae4e
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-09-30 11:00:05 +0000

    Modified log4j settings for production environment

--- trunk/menuplanner/grails-app/conf/Config.groovy
+++ trunk/menuplanner/grails-app/conf/Config.groovy
@@ -209,9 +209,14 @@ log4j = {
     // Example of changing the log pattern for the default console
     // appender:
     //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
+    appenders {
+		environments {
+			production {
+				rollingFile name: "myAppender", maxFileSize: 26214400, maxBackupIndex: 10, file: "/var/log/tomcat6/minutemenu.log"
+				rollingFile name: "stacktrace", maxFileSize: 26214400, file: "/var/log/tomcat6/stacktrace.log"
+			}
+		}
+	}
 
 
     error 'org.codehaus.groovy.grails.web.servlet',  //  controllers
