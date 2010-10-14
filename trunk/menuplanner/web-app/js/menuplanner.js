8667fefae3455ed401025cb7a4d59caeae33003f
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2010-10-14 18:46:01 +0000

    #158 Shrinking border margin to fit

--- trunk/menuplanner/web-app/js/menuplanner.js
+++ trunk/menuplanner/web-app/js/menuplanner.js
@@ -35,14 +35,16 @@ function scaleImageSize() {
     if (imgRatio > divRatio) {
         jQuery('.scaleImageSize img').attr('height', divH);
         imgW = jQuery('.scaleImageSize img').width();
+        jQuery('.scaleImageSize').width(imgW);
         var marginLeft = (divW - imgW) / 2
-        jQuery('.scaleImageSize img').css('margin-left', marginLeft);
+        jQuery('.scaleImageSize').css('margin-left', marginLeft);
     }
     else {
         jQuery('.scaleImageSize img').attr('width', divW);
         imgH = jQuery('.scaleImageSize img').height();
-        var marginTop = (divH - imgH) / 2
-        jQuery('.scaleImageSize img').css('margin-top', marginTop);
+        jQuery('.scaleImageSize').height(imgH)
+//        var marginTop = (divH - imgH) / 2
+//        jQuery('.scaleImageSize img').css('margin-top', marginTop);
     }
 }
 
