23bfda8b6af8e61d70d46095fde04daf70f43c6c
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2011-02-24 09:33:09 +0000

    Fixed Unit Tests

--- trunk/menuplanner/test/unit/com/mp/domain/ItemTests.groovy
+++ trunk/menuplanner/test/unit/com/mp/domain/ItemTests.groovy
@@ -7,40 +7,35 @@ import org.codehaus.groovy.grails.commons.ConfigurationHolder
 import static com.mp.MenuConstants.*
 
 class ItemTests extends GrailsUnitTestCase {
+    Party party1
+    Party party2
+    Party partyAlcoholic
+    Party partyNonAlcoholic
+
     protected void setUp() {
         super.setUp()
         mockConfig '''bootstrapMode:true'''
-        def recipeInstances = []
-        def instances = []
-        def productInstances = []
-        def measurableProductInstances = []
-
-        def unitInstances = [new Time(name: TIME_UNIT_MINUTES, symbol: TIME_UNIT_MINUTES_SYMBOL,
-              definition: "This is definition for minutes", metricType: MetricType.TIME)]
-        def quantityInstances = []
         Object.metaClass.s = MenuConstants.s
-
-        mockDomain(Quantity, quantityInstances)
-        mockDomain(Unit, unitInstances)
-        mockDomain(Recipe, recipeInstances)
-        mockDomain(Item, instances)
-        mockDomain(Product, productInstances)
-        mockDomain(MeasurableProduct, measurableProductInstances)
-        def riInstances = []
-        mockDomain(RecipeIngredient, riInstances)
-        def partyInstances = []
-        mockDomain(Party, partyInstances)
+        mockDomain(Quantity)
+        mockDomain(Unit)
+        mockDomain(Recipe)
+        mockDomain(Item)
+        mockDomain(Product)
+        mockDomain(MeasurableProduct)
+        mockDomain(RecipeIngredient)
+        mockDomain(Party)
         mockDomain(Aisle)
-        Party party1 = new Party(name: "party_1")
+        party1 = new Party(name: "party_1")
         party1.save()
-        Party party2 = new Party(name: "party_2")
+        party2 = new Party(name: "party_2")
         party2.save()
-        Party partyAlcoholic = new Party(name: "party_Alcoholic", showAlcoholicContent: true)
+        partyAlcoholic = new Party(name: "party_Alcoholic", showAlcoholicContent: true)
         partyAlcoholic.save()
-        Party partyNonAlcoholic = new Party(name: "party_Alcoholic", showAlcoholicContent: false)
+        partyNonAlcoholic = new Party(name: "party_Alcoholic", showAlcoholicContent: false)
         partyNonAlcoholic.save()
         Item item = new Item(name: "TestItem")
         item.save()
+        Product.metaClass.isProductAlcoholic = {return delegate.isAlcoholic}
     }
 
 
@@ -57,15 +52,12 @@ class ItemTests extends GrailsUnitTestCase {
       assert item
     }
     void test_getItemsForUser_Valid_Recipes() {
-        Party party1 = Party.get(1)
         createRecipeInstances(10, party1)
         List<Item> recipeByUser = Item.getItemsForUser(party1)
         assertEquals "Unable to create recipes for user", 10, recipeByUser.size()
     }
 
     void test_getItemsForUser_First_User_Recipe_Invisible_To_Another() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         def recs = createRecipeInstances(10, party1)
         assertEquals "Recipes weren't created",10,recs.size()
         List<Item> recipeByUser = Item.getItemsForUser(party1)
@@ -75,8 +67,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_First_User_Recipe_Visible_To_Another() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         def recs = createRecipeInstances(10, party1,true)
         assertEquals "Recipes weren't created",10,recs.size()
         List<Item> recipeByUser = Item.getItemsForUser(party1)
@@ -86,14 +76,12 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_Products_Visible_ToUser() {
-        Party party1 = Party.get(1)
         createProductInstances(10, party1)
         List<Item> productByUser = Item.getItemsForUser(party1)
         assertEquals "Unable to create products for user", 10, productByUser.size()
     }
 
     void test_getItemsForUser_Products_Like_Query() {
-        Party party1 = Party.get(1)
         createProductInstances_Diff_Name(party1)
         List<Item> butterProducts = Item.getItemsForUser(party1, '%But%')
         assertEquals "Unable to create products-Butter for user", 3, butterProducts.size()
@@ -102,7 +90,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_Products_Ilike_Query() {
-        Party party1 = Party.get(1)
         createProductInstances_Diff_Name(party1)
         List<Item> butterProducts = Item.getItemsForUser(party1, '%but%')
         assertEquals "Unable to create products-Butter for user", 3, butterProducts.size()
@@ -111,8 +98,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_Products_InVisible_To_Other_User() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         createProductInstances(10, party1)
         createProductInstances(20, party2)
         List<Item> productByUser1 = Item.getItemsForUser(party1)
@@ -122,22 +107,18 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_Items_Visible_To_All() {
-        Party party1 = Party.get(1)
         createItemInstances(10, true)
         List<Item> items = Item.getItemsForUser(party1)
         assertEquals "Unable to create Items ", 10, items.size()
     }
 
     void test_getItemsForUser_Items_Invisible_To_All() {
-        Party party1 = Party.get(1)
         createItemInstances(10)
         List<Item> items = Item.getItemsForUser(party1)
         assertEquals "Unable to create Items ", 0, items.size()
     }
 
     void test_getItemsForUser_All_User_Items() {
-        Party party1 = Party.get(2)
-        Party party2 = Party.get(2)
         createItemInstances(20, true)
         List<Item> items1 = Item.getItemsForUser(party1)
         List<Item> items2 = Item.getItemsForUser(party2)
@@ -146,7 +127,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_All() {
-        Party party1 = Party.get(1)
         createRecipeInstances(10, party1)
         createProductInstances(10, party1)
         createItemInstances(20, true)
@@ -156,7 +136,6 @@ class ItemTests extends GrailsUnitTestCase {
 
 
     void test_getItemsForUser_All_Items_Alcoholic() {
-        Party partyAlcoholic = Party.get(3)
         createProductInstances(10, partyAlcoholic, true)
         createItemInstances(20, true)
         List<Item> totalItems = Item.getItemsForUser(partyAlcoholic)
@@ -164,7 +143,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_All_Items_Non_Alcoholic() {
-        Party partyAlcoholic = Party.get(3)
         createProductInstances(10, partyAlcoholic, false)
         createItemInstances(20, true)
         List<Item> totalItems = Item.getItemsForUser(partyAlcoholic)
@@ -172,7 +150,8 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_All_Items_Alcoholic_Party_Non_Alcoholic() {
-        Party partyNonAlcoholic = Party.get(4)
+        partyNonAlcoholic.ingredients = []
+        partyNonAlcoholic.contributions = []
         createProductInstances(10, partyNonAlcoholic, true)
         createItemInstances(20, true, true)
         List<Item> totalItems = Item.getItemsForUser(partyNonAlcoholic)
@@ -180,7 +159,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_All_Items_NonAlcoholic_Party_NonAlcoholic() {
-        Party partyNonAlcoholic = Party.get(4)
         createProductInstances(10, partyNonAlcoholic, false)
         createItemInstances(20, true, false)
         List<Item> totalItems = Item.getItemsForUser(partyNonAlcoholic)
@@ -188,37 +166,30 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getItemsForUser_Recipe_Alcoholic_Party_NonAlcoholic() {
-        Party partyNonAlcoholic = Party.get(4)
         createRecipeInstances(10, partyNonAlcoholic, true, true)
         List<Item> totalItems = Item.getItemsForUser(partyNonAlcoholic)
         assertEquals "Got wrong recipes for user", 0, totalItems.size()
     }
 
     void test_getItemsForUser_Recipe_Non_Alcoholic_Party_NonAlcoholic() {
-        Party partyNonAlcoholic = Party.get(4)
         createRecipeInstances(10, partyNonAlcoholic, true, false)
         List<Item> totalItems = Item.getItemsForUser(partyNonAlcoholic)
         assertEquals "Got wrong recipes for user", 10, totalItems.size()
     }
 
     void test_getProductsForUser_Only_Recipes() {
-        Party party1 = Party.get(1)
         createRecipeInstances(10, party1)
         List<Item> result = Item.getProductsForUser(party1)
         assertEquals "Found recipes(wrong items) for user", 0, result.size()
     }
 
     void test_getProductsForUser_Only_Recipes_By_Other_User() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         createRecipeInstances(10, party2)
         List<Item> result = Item.getProductsForUser(party1)
         assertEquals "Found recipes(wrong items) for user", 0, result.size()
     }
 
     void test_getProductsForUser_Only_Recipes_By_Both_Users() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         createRecipeInstances(10, party1)
         createRecipeInstances(10, party2)
         List<Item> result = Item.getProductsForUser(party1)
@@ -226,23 +197,18 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getProductsForUser_Only_Recipes_By_Other_User_Share_With_Community() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         createRecipeInstances(10, party2, true)
         List<Item> result = Item.getProductsForUser(party1)
         assertEquals "Found recipes(wrong items) for user", 0, result.size()
     }
 
     void test_getProductsForUser_Only_Products() {
-        Party party1 = Party.get(1)
         createProductInstances(10, party1)
         List<Item> result = Item.getProductsForUser(party1)
         assertEquals "Unable to get products for user", 10, result.size()
     }
 
     void test_getProductsForUser_Another_User_Product() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         createProductInstances(10, party1)
         createProductInstances(10, party2)
         List<Item> result1 = Item.getProductsForUser(party1)
@@ -252,7 +218,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getProductsForUser_Products_Like_Query() {
-        Party party1 = Party.get(1)
         createProductInstances_Diff_Name(party1)
         List<Item> butterProducts = Item.getProductsForUser(party1, '%But%')
         assertEquals "Unable to create products-Butter for user", 3, butterProducts.size()
@@ -261,7 +226,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getProductssForUser_Products_Ilike_Query() {
-        Party party1 = Party.get(1)
         createProductInstances_Diff_Name(party1)
         List<Item> butterProducts = Item.getProductsForUser(party1, '%but%')
         assertEquals "Unable to create products-Butter for user", 3, butterProducts.size()
@@ -270,8 +234,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getProductsForUser_All_User_Products() {
-        Party party1 = Party.get(1)
-        Party party2 = Party.get(2)
         createProductInstances(20, party1, true)
         createProductInstances(10, party2, false)
         List<Item> items1 = Item.getProductsForUser(party1)
@@ -281,14 +243,12 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getProductssForUser_All_Products_Alcoholic() {
-        Party partyAlcoholic = Party.get(3)
         createProductInstances(10, partyAlcoholic, true)
         List<Item> totalItems = Item.getProductsForUser(partyAlcoholic)
         assertEquals "Unable to get Items for user", 10, totalItems.size()
     }
 
     void test_getProductsForUser_Products_Mixed_Party_Alcoholic() {
-        Party partyAlcoholic = Party.get(3)
         createProductInstances(10, partyAlcoholic, false)
         createProductInstances(20, partyAlcoholic, true)
         List<Item> totalItems = Item.getProductsForUser(partyAlcoholic)
@@ -296,7 +256,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getProductsForUser_Products_Mixed_Party_Non_Alcoholic() {
-        Party partyNonAlcoholic = Party.get(4)
         createProductInstances(10, partyNonAlcoholic, false)
         createProductInstances(20, partyNonAlcoholic, true)
         List<Item> totalItems = Item.getProductsForUser(partyNonAlcoholic)
@@ -304,7 +263,6 @@ class ItemTests extends GrailsUnitTestCase {
     }
 
     void test_getProductsForUser_All_Products_Alcoholic_Party_Non_Alcoholic() {
-        Party partyNonAlcoholic = Party.get(4)
         createProductInstances(10, partyNonAlcoholic, true)
         createItemInstances(20, true, true)
         List<Item> totalItems = Item.getProductsForUser(partyNonAlcoholic)
