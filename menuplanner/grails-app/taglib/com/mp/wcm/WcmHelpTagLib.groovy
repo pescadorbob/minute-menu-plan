89a2e4e8050400b5636c251b9799fb901d6082e7
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2011-02-24 11:26:08 +0000

    Handled Null Pointer Exception

--- menuplanner/grails-app/taglib/com/mp/wcm/WcmHelpTagLib.groovy
+++ menuplanner/grails-app/taglib/com/mp/wcm/WcmHelpTagLib.groovy
@@ -9,19 +9,21 @@ import com.mp.tools.UserTools
 import org.weceem.html.WcmHTMLContent
 
 public class WcmHelpTagLib {
-  static namespace = "wcmhelp"
-  def wcmContentRepositoryService
+    static namespace = "wcmhelp"
+    def wcmContentRepositoryService
 
-     def eachPage = { attrs,body ->
-       def var = attrs.var ? attrs.var : "content"
-         def pages  = wcmContentRepositoryService.findAllContent(wcmContentRepositoryService.findDefaultSpace())
-         pages.each {
-             if(it instanceof WcmHTMLContent && it.status.getPublicContent()){
-               out << body((var): it)
-             }
+    def eachPage = { attrs, body ->
+        def var = attrs.var ? attrs.var : "content"
+        def space = wcmContentRepositoryService.findDefaultSpace()
+        if (space) {
+            def pages = wcmContentRepositoryService.findAllContent(wcmContentRepositoryService.findDefaultSpace())
+            pages.each {
+                if (it instanceof WcmHTMLContent && it.status.getPublicContent()) {
+                    out << body((var): it)
+                }
 //     out << """${ConfigurationHolder.config.weceem.content.prefix}${content.absoluteURI}">${content.title}</a>"""
 
-         }
-
-     }
+            }
+        }
+    }
 }
