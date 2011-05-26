37b45e047bcd645cab4eb7d8a9c7a980191551a8
aman <aman@ea6f632a-2a3d-410d-a33f-4c9a882c5acb>
2011-05-26 06:31:41 +0000

    updated the backup script

--- trunk/menuplanner/misc/backupDataToS3.sh
+++ trunk/menuplanner/misc/backupDataToS3.sh
@@ -16,13 +16,22 @@ tarBackUpFolder() {
 copyFileToS3() {
 	s3cmd put $1 s3://mmpbucket/sqlDumps/
 }
+log() {
+	echo $1 >> $LOG_FILE
+}
+
+LOG_FILE=~/backupLogs/logFile.txt
+log "Back up Started at `date`"
 BACK_UP_FOLDER=/home/ubuntu/sqldumps/menuplanner`date +%a_%H`
 TAR_FILE=/home/ubuntu/sqldumps/tar_menuplannerDb_`date +%a_%H`".tar.gz"
 sudo rm -rf $BACK_UP_FOLDER
 mkdir -p $BACK_UP_FOLDER
 takeDbBackUp "menuplanner"
 takeFolderBackup "/home/ubuntu/mpImages"
+log "Back ups moved to $BACK_UP_FOLDER at `date`"
 cd $BACK_UP_FOLDER
+log "creating tar at `date`"
 tarBackUpFolder . $TAR_FILE
+log "Copying tar to s3 at `date`"
 copyFileToS3 $TAR_FILE
-
+log "Back up Ended at `date`"
